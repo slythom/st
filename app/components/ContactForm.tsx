e1a785d211816cc3a24ba7dc29bfb6c8fa829365
slythom <contact@sylvainthomas.fr>
2024-10-13 22:21:00 +0200

    contact form updates

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { contactFormAction } from "../actions/contactFormAction";
 import ReCaptcha from "./ReCaptcha";
+import { useFormStatus, useFormState } from 'react-dom'
 
 const ContactForm: React.FC = () => {
   const [reCaptchaToken, setReCaptchaToken] = useState<string>("");
@@ -11,27 +12,41 @@ const ContactForm: React.FC = () => {
     setReCaptchaToken(token);
   };
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    formData.append("recaptchaToken", reCaptchaToken);
+  const { pending } = useFormStatus()
 
-    try {
-      const result = await contactFormAction(formData);
-      // Gérer le résultat (par exemple, afficher un message de succès)
-      console.log("Form submission result:", result);
-    } catch (error) {
-      // Gérer les erreurs
-      console.error("Form submission error:", error);
-    }
-  };
+  // const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   const formData = new FormData(event.currentTarget);
+  //   formData.append("recaptchaToken", reCaptchaToken);
+
+  //   try {
+  //     const result = await contactFormAction(formData);
+  //     // Gérer le résultat (par exemple, afficher un message de succès)
+  //     console.log("Form submission result:", result);
+  //   } catch (error) {
+  //     // Gérer les erreurs
+  //     console.error("Form submission error:", error);
+  //   }
+  // };
+
+  const initialState = {
+    message: '',
+    success: false
+  }
+
+  const [state, formAction] = useFormState(contactFormAction, initialState)
 
   return (
     <div className="p-6 md:p-10 mb-20 md:mb-0 mx-auto max-w-4xl bg-white font-[sans-serif]">
       <h1 className="text-3xl text-gray-800 font-extrabold text-center">
         Me contacter
       </h1>
-      <form className="mt-12 space-y-6" onSubmit={handleSubmit}>
+      {state.message && (
+        <div className={`mt-4 p-4 rounded ${state.success ? 'bg-green-100 text-green-700' : 'bg-red-100 text-red-700'}`}>
+          {state.message}
+        </div>
+      )}
+      <form className="mt-12 space-y-6" action={formAction}>
         <div>
           <label
             htmlFor="sender_name"
@@ -81,10 +96,11 @@ const ContactForm: React.FC = () => {
           ></textarea>
         </div>
 
+        <input type="hidden" name="recaptchaToken" value={reCaptchaToken} />
         <ReCaptcha onVerify={handleRecaptchaVerify} />
 
         <button
-          type="submit"
+          type="submit" disabled={pending}
           className="w-full rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
         >
           Envoyer
